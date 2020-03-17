cfac83fcc2968e6e6eab584e93bcac2c6e496dbe
dheker-benamor <57073332+dheker-benamor@users.noreply.github.com>
2020-03-17 23:47:27 +0100

    Product Type crud

---
+++ routes/productType.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+var ProductTypeService = require('../services/ProductType.service');
+
+
+
+// get all Product Types
+router.get('/', ProductTypeService.getAllTypes);
+
+// create a new product type
+router.post('/', ProductTypeService.Create);
+
+// get a type by id
+router.get('/:typeId', ProductTypeService.getById);
+
+// delete a product type
+router.delete('/:typeId', ProductTypeService.delete);
+
+// update a product type
+router.put('/:typeId', ProductTypeService.update);
+
+module.exports = router;
---
+++ services/ProductType.service.js
@@ -0,0 +1,57 @@
+const ProductType = require('../models/ProductType.model');
+
+// get all Product Types
+exports.getAllTypes =   (req, res) =>{
+   ProductType.find()
+        .then(productTypes => res.json(productTypes))
+        .catch(err => res.status(400).json('Error: ' + err));
+
+};
+
+// create a new product type
+exports.Create = async  (req, res) => {
+    const type = new ProductType({
+        name: req.body.name,
+        description: req.body.description
+    });
+    try {
+        const savedProductType = await type.save();
+        res.json(savedProductType);
+    } catch (err) {
+        res.json({message: err});
+    }
+};
+
+// get a type by id
+exports.getById = async  (req, res) => {
+    try {
+        const type = await ProductType.findById(req.params.typeId);
+        res.json(type);
+    } catch (err) {
+        res.json({message: err});
+    }
+};
+
+// delete a product type
+exports.delete = async  (req, res) => {
+    try {
+        const removed = await ProductType.remove({_id: req.params.typeId});
+        res.json(removed);
+    } catch (err) {
+        res.json({message: err});
+    }
+};
+
+// update a product type
+exports.update = async  (req, res) => {
+    try {
+        const updated = await ProductType.updateOne(
+            { _id: req.params.typeId },
+            { $set: { description: req.body.description }}
+        );
+        res.json(updated);
+    } catch (err) {
+        res.json({message: err});
+    }
+};
+
