3040fea18745c8e70ca1bd51d877400433ed41ce
dheker-benamor <57073332+dheker-benamor@users.noreply.github.com>
2020-03-27 00:28:12 +0100

    product sub-type service (frontend)

--- services/ProductType.service.js
+++ services/ProductType.service.js
@@ -1,5 +1,7 @@
 const ProductType = require('../models/ProductType.model');
 const Page = require('../models/page.model');
+const Website = require('../models/website.model');
+const Link = require('../models/link.model');
 
 // get all Product Types
 exports.getAllTypes =   (req, res) =>{
@@ -58,11 +60,26 @@ exports.update = async  (req, res) => {
 
 // assign a product type to a website
 exports.assignTypeToWebsite = async  (req, res) => {
-    const saved = await Page.create(req.body).then((s)=>res.json(s)).catch(err => res.status(400).json('Error: ' + err));
+    const saved = await Page.create(req.body)
+        .then(async (s)=>{
+            await  res.json(s);
+            const link = new Link({
+                link_text: s.page_name,
+                page: s._id
+            });
+
+            await link.save(
+                await Website.updateOne(
+                    { _id: req.body.website },
+                    { $push: { pages : s._id , 'header.links' : link._id }}
+                )
+            )
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
 };
 // get  Product Types by website
 exports.getByWebsite =   (req, res) =>{
-    Page.find({website:req.params.websiteId})
+    Page.find({website:req.params.websiteId,type:"category"})
         .then(pages => {
             const types =[];
             pages.forEach( async  (page,i) =>{
@@ -83,7 +100,7 @@ exports.getByWebsite =   (req, res) =>{
 };
 // get  Product Types pages by website
 exports.getTypesPagesByWebsite =   (req, res) =>{
-    Page.find({website:req.params.websiteId})
+    Page.find({website:req.params.websiteId,type:"category"})
         .then(pages => {
 
             res.json(pages);
