230d17f280e53777ae7774f4f9eabccb707c8794
youssefMhe <56787023+youssefMhe@users.noreply.github.com>
2020-04-03 03:57:06 +0100

    add chatbot +static ads banner to front

--- client/src/views/website/website.js
+++ client/src/views/website/website.js
@@ -4,6 +4,8 @@ import serviceSite from '../../services/website.service'
 import servicePage from '../../services/page.service'
 import Category from "../../theme/theme1/views/category";
 import NavTools from "../../components/Navbars/NavTools";
+import Ads from "../../components/Ads/Ads"
+import ChatBot from 'react-simple-chatbot';
 
 
 class Website extends React.Component {
@@ -59,6 +61,7 @@ class Website extends React.Component {
          return (
             <div className="wrapper">
                 <NavTools/>
+                <Ads/>
                 <Suspense fallback={<div>Loading ...</div>}>
                     {  website.header && pages ? this.loadHeader(): ''}
                 </Suspense>
@@ -67,6 +70,56 @@ class Website extends React.Component {
                         { router }
                     </Suspense>
                 </div>
+                <ChatBot
+                    steps={[
+
+                        {
+                            id: '1',
+                            message: 'What is your name?',
+                            trigger: '2',
+                        },
+                        {
+                            id: '2',
+                            user: true,
+                            trigger: '3',
+                        },
+                        {
+                            id: '3',
+                            message: 'Hi {previousValue}, nice to meet you!',
+                            trigger: '4',
+                        },
+
+                        {
+                            id: '4',
+                            message: 'What ur occupation?',
+                            trigger: '5',
+                        },
+                        {
+                            id: '5',
+                            options: [
+                                { value: 1, label: 'student', trigger: '6' },
+                                { value: 2, label: 'businessman', trigger: '7' },
+                                { value: 3, label: 'normal employee', trigger: '8' },
+                            ],
+                        },
+                        {
+                            id: '6',
+                            message: 'you are a student, so  we have some wonderful bank offer that can give u many advantage',
+                            end: true,
+                        },
+                        {
+                            id: '7',
+                            message: 'you are a businessman,  great so  we have some Special bank offer that can give u many advantage',
+                            end: true,
+                        },
+                        {
+                            id: '8',
+                            message: 'you are a normal employee ,  so  we have some Special bank offer that can help and give u many advantage',
+                            end: true,
+                        },
+
+                    ]}
+                />
             </div>
         );
     }
