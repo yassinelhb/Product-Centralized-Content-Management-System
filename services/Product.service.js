d52cecbe151fe187ef27f393473eea4dc418bc54
dheker-benamor <57073332+dheker-benamor@users.noreply.github.com>
2020-04-05 21:47:26 +0100

    add product Pages + find products by website +find products by country

--- services/Product.service.js
+++ services/Product.service.js
@@ -1,23 +1,85 @@
+import productProperties from "../client/src/views/productProperty/productProperties";
+const Page = require('../models/page.model');
+const Website = require('../models/website.model');
+const Link = require('../models/link.model');
+const SubType = require('../models/ProductSubType.model');
+const Layout = require('../models/layout.model');
 const Product = require('../models/Product.model');
 
-// get all Product
+// get all Products
 exports.getAll =   (req, res) =>{
 
-    Product.find()
+    Product.find().populate('subType')
               .then(products => res.json(products))
               .catch(err => res.status(400).json('Error: ' + err));
 
 };
+// get all Products by country
+exports.findByCountry =   (req, res) =>{
 
+    Product.find({country_code:req.params.country}).populate('subType')
+        .then(products => res.json(products))
+        .catch(err => res.status(400).json('Error: ' + err));
+
+};
+// get all Products by website
+exports.findByWebsite =   (req, res) =>{
+    Page.find({website:req.params.websiteId,type:"product"}).populate('subType')
+        .then(pages => {
+            const products =[];
+            pages.forEach( async  (page,i) =>{
+                    const product = await Product.findById(page.get('product'));
+                    products.push(product);
+
+                    if(i === pages.length -1){
+
+                        await res.json(products);
+                    }
+                }
+            );
+
+
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+
+};
+// get  Product Types pages by website
+exports.getPagesByWebsite =   (req, res) =>{
+    Page.find({website:req.params.websiteId,type:"detail"})
+        .then(pages => {
+
+            res.json(pages);
+
+
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+
+};
 // create a new product
 exports.create = async  (req, res) => {
 
-    try {
-        const saved = await Product.create(req.body);
-        res.json(saved);
-    } catch (err) {
-        res.json({message: err});
-    }
+
+        const saved = await Product.create(req.body)
+            .then( product =>
+                {
+                    const productLayout =  Layout.findOne({website:req.params.websiteId,layout_name:'detail'}).then().catch();
+
+                    const b ={
+
+                        "page_name":product.name,
+                        "type":"product",
+                        "productSubType":product.subType,
+                        "product":product._id,
+                        "website":req.params.websiteId,
+                        "layout":productLayout._id
+                    };
+                    Page.create(req.body).then().catch();
+                    res.json(product);
+                }
+
+            )
+            .catch(err => res.status(400).json('Error: ' + err));
+
 };
 
 // get a product by id
